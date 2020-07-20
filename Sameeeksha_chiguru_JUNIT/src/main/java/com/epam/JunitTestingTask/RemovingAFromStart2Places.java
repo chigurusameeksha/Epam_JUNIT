6039989c01bc938b77823b2bcbc7894e855af1d6
SameekshaChiguru <sameekshachiguru@gmail.com>
2020-07-20 08:54:32 +0530

    first commit

---
+++ Sameeeksha_chiguru_JUNIT/src/main/java/com/epam/JunitTestingTask/RemovingAFromStart2Places.java
@@ -0,0 +1,21 @@
+package com.epam.JunitTestingTask;
+
+public class RemovingAFromStart2Places 
+{
+    String removingA(String originalString)
+    {
+    	String finalString="";
+    	for(int index=0;index<originalString.length();index++)
+    	{
+    		if(checkConditionAAtFirstTwoPositions(originalString.charAt(index),index))
+    		{
+    			finalString+=originalString.charAt(index);
+    		}
+    	}
+    	return finalString;
+    }
+    boolean checkConditionAAtFirstTwoPositions(Character stringIndexChar,Integer indexOfChar)
+    {
+    	return (stringIndexChar!='A')||(stringIndexChar=='A' && indexOfChar>1);
+    }
+}
